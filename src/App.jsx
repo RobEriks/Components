78a9c0c0089ffd4fc56dba43fe5f4d4208d78247
RobEriks <Rob.Eriksson@outlook.com>
2025-01-05 00:45:32 +0100

    U

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Buttons from './Buttons'
+import Button from './Buttons'
 
 function App() {
   
 
   return (
-    <>
-      <Buttons/>
-    </>
+    <div style={{ padding: '10px'}}>
+      <h1>Buttons</h1>
+      <Button/>
+      </div>
   )
 }
 
